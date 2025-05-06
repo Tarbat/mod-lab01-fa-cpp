9913777249d216803c50cea8bf0fd4103736f9b9
Tarbat <vitaliy.paymuhin@mail.ru>
2025-05-06 14:49:07 +0300

    eleven version 2

--- src/fun.cpp
+++ src/fun.cpp
@@ -30,7 +30,9 @@ unsigned int faStr2(const char* str) {
 
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != ' ' && word == true) {
-            if (!isdigit(str[i]) && !ispunct(str[i])) letter = true;
+            if (!isdigit(str[i]) && !ispunct(str[i])) {
+                letter = true;
+            }
             else {
                 letter = false;
             }
